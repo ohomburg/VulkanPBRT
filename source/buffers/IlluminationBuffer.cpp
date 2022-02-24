074c84c4ad6f35b70a124da215b6a6c388f9188a
Oskar Homburg <oskar@ohomburg.de>
2022-02-24 18:01:08 +0100

    Use 32bpc for final buffer

--- source/buffers/IlluminationBuffer.cpp
+++ source/buffers/IlluminationBuffer.cpp
@@ -79,7 +79,7 @@ void IlluminationBufferFinal::fillImages()
 {
     auto image = vsg::Image::create();
     image->imageType = VK_IMAGE_TYPE_2D;
-    image->format = VK_FORMAT_B8G8R8A8_UNORM;
+    image->format = VK_FORMAT_R32G32B32A32_SFLOAT;
     image->extent.width = width;
     image->extent.height = height;
     image->extent.depth = 1;
