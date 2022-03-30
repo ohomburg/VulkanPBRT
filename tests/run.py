c3aca3eb843ec3777b8d4f70ba2d45824dd0e835
Oskar Homburg <oskar@ohomburg.de>
2022-03-30 21:09:41 +0200

    Add more test configs

--- tests/run.py
+++ tests/run.py
@@ -33,20 +33,63 @@ def set_spp(spp):
 
 
 configs = {
-    "reference": {"--vptLimit": 2**31 - 1, "--spp": 1024},
+    "reference": {"--vptLimit": 2 ** 31 - 1, "--spp": 1024},
+    "fast": {"--denoiser": "asvgf", "--vptLimit": 1024, "--cloudReproPoints": 1, "--cloudStatSteps": 64, "--vptBundle": 1, "--atrousIters": 3, "--tempAlpha": "0.01"},
+    "balanced": {"--denoiser": "asvgf"},
+    "quality": {"--denoiser": "asvgf", "--vptLimit": 2 ** 31 - 1, "--cloudReproPoints": 1, "--cloudStatSteps": 256, "--vptBundle": 2, "--atrousIters": 5, "--tempAlpha": "0.01"},
+
+    "bundle2": {"--denoiser": "asvgf", "--vptBundle": 2},
+    "bundle3": {"--denoiser": "asvgf", "--vptBundle": 3},
+    "bundle4": {"--denoiser": "asvgf", "--vptBundle": 4},
+    "bundle2fast": {"--denoiser": "asvgf", "--vptLimit": 1024, "--vptBundle": 2},
+    "bundle4fast": {"--denoiser": "asvgf", "--vptLimit": 1024, "--vptBundle": 4},
+
     "limit512": {"--vptLimit": 512} | set_spp(256),
     "limit768": {"--vptLimit": 768} | set_spp(256),
     "limit1024": {"--vptLimit": 1024} | set_spp(256),
     "limit1536": {"--vptLimit": 1536} | set_spp(256),
     "limit2048": {"--vptLimit": 2048} | set_spp(256),
+    "limit768d": {"--denoiser": "asvgf", "--vptLimit": 768},
+    "limit1024d": {"--denoiser": "asvgf", "--vptLimit": 1024},
+    "limit1536d": {"--denoiser": "asvgf", "--vptLimit": 1536},
+    "limit2048d": {"--denoiser": "asvgf", "--vptLimit": 2048},
+
+    "statsteps16": {"--denoiser": "asvgf", "--cloudStatSteps": 16},
+    "statsteps32": {"--denoiser": "asvgf", "--cloudStatSteps": 32},
+    "statsteps64": {"--denoiser": "asvgf", "--cloudStatSteps": 64},
+    "statsteps96": {"--denoiser": "asvgf", "--cloudStatSteps": 96},
+    "statsteps128": {"--denoiser": "asvgf", "--cloudStatSteps": 128},
+    "statsteps192": {"--denoiser": "asvgf", "--cloudStatSteps": 192},
+    "statsteps256": {"--denoiser": "asvgf", "--cloudStatSteps": 256},
+    "statsteps384": {"--denoiser": "asvgf", "--cloudStatSteps": 384},
+
+    "repro1": {"--denoiser": "asvgf", "--cloudReproPoints": 1},
+    "repro3": {"--denoiser": "asvgf", "--cloudReproPoints": 3},
+    "repro5": {"--denoiser": "asvgf", "--cloudReproPoints": 5},
+
     "alpha1e-1": {"--denoiser": "asvgf", "--atrousIters": 0, "--tempAlpha": "0.1"},
     "alpha5e-2": {"--denoiser": "asvgf", "--atrousIters": 0, "--tempAlpha": "0.05"},
     "alpha1e-2": {"--denoiser": "asvgf", "--atrousIters": 0, "--tempAlpha": "0.01"},
     "alpha5e-3": {"--denoiser": "asvgf", "--atrousIters": 0, "--tempAlpha": "0.005"},
+
+    "iters0": {"--denoiser": "asvgf", "--atrousIters": 0},
+    "iters1": {"--denoiser": "asvgf", "--atrousIters": 1},
+    "iters2": {"--denoiser": "asvgf", "--atrousIters": 2},
+    "iters3": {"--denoiser": "asvgf", "--atrousIters": 3},
+    "iters4": {"--denoiser": "asvgf", "--atrousIters": 4},
+    "iters5": {"--denoiser": "asvgf", "--atrousIters": 5},
+    "iters6": {"--denoiser": "asvgf", "--atrousIters": 6},
+
+    "filter_box3": {"--denoiser": "asvgf", "--atrousFilter": 1},
+    "filter_gauss5": {"--denoiser": "asvgf", "--atrousFilter": 0},
+    "filter_box5": {"--denoiser": "asvgf", "--atrousFilter": 2},
+    "filter_sub": {"--denoiser": "asvgf", "--atrousFilter": 3},
+    "filter_sub3": {"--denoiser": "asvgf", "--atrousFilter": 4},
+    "filter_sub5": {"--denoiser": "asvgf", "--atrousFilter": 5},
 }
 
 for name, overrides in configs.items():
-    disabled_cases = configs.get("disabled", [])
+    disabled_cases = overrides.get("disabled", [])
     for i, base_args in enumerate(cases):
         if i in disabled_cases:
             continue
@@ -82,4 +125,5 @@ refdir = Path(os.getcwd(), "reference")
 for name, overrides in configs.items():
     outdir = Path(os.getcwd(), name)
     for file in outdir.glob("*.exr"):
-        subprocess.run(["flip", "-r", Path(refdir, file.name), "-t", file, "-d", outdir, "-nexm", "-c", file.with_suffix(".flip.csv")])
+        subprocess.run(["flip", "-r", Path(refdir, file.name), "-t", file, "-d", outdir, "-nexm", "-c",
+                        file.with_suffix(".flip.csv")])
